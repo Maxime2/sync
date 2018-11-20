e994ef19ee9b204335d0a58145c638f5379824ab
Maxim Zakharov <dp.maxime@gmail.com>
2018-11-20 15:05:47 +1100

    add caller info

--- mutex.go
+++ mutex.go
@@ -3,6 +3,7 @@
 package sync
 
 import (
+	"runtime"
 	"sync"
 	"github.com/sirupsen/logrus"
 )
@@ -19,25 +20,39 @@ func (m *Mutex_trace) SetNameAndLogger(name string, logger *logrus.Entry) {
 }
 
 func (m *Mutex_trace) Lock() {
+	pc, filename, line, ok := runtime.Caller(1)
 	m.logger.WithFields(logrus.Fields{
 		"name": m.name,
 		"status": "request",
+		"file": filename,
+		"line" : line,
+		"ok": ok,
 	}).Warn("Lock()")
 	m.mu.Lock()
 	m.logger.WithFields(logrus.Fields{
 		"name": m.name,
 		"status": "grant",
+		"file": filename,
+		"line" : line,
+		"ok": ok,
 	}).Warn("Lock()")
 }
 
 func (m *Mutex_trace) Unlock() {
+	pc, filename, line, ok := runtime.Caller(1)
 	m.logger.WithFields(logrus.Fields{
 		"name": m.name,
 		"status": "request",
+		"file": filename,
+		"line" : line,
+		"ok": ok,
 	}).Warn("Unlock()")
 	m.mu.Unlock()
 	m.logger.WithFields(logrus.Fields{
 		"name": m.name,
 		"status": "grant",
+		"file": filename,
+		"line" : line,
+		"ok": ok,
 	}).Warn("Unlock()")
 }
--- rwmutex.go
+++ rwmutex.go
@@ -3,6 +3,7 @@
 package sync
 
 import (
+	"runtime"
 	"sync"
 	"github.com/sirupsen/logrus"
 )
@@ -19,49 +20,77 @@ func (rw *RWMutex_trace) SetNameAndLogger(name string, logger *logrus.Entry) {
 }
 
 func (rw *RWMutex_trace) RLock() {
+	pc, filename, line, ok := runtime.Caller(1)
 	rw.logger.WithFields(logrus.Fields{
 		"name": rw.name,
 		"status": "request",
+		"file": filename,
+		"line" : line,
+		"ok": ok,
 	}).Warn("RLock()")
 	rw.mu.RLock()
 	rw.logger.WithFields(logrus.Fields{
 		"name": rw.name,
 		"status": "grant",
+		"file": filename,
+		"line" : line,
+		"ok": ok,
 	}).Warn("RLock()")
 }
 
 func (rw *RWMutex_trace) RUnlock() {
+	pc, filename, line, ok := runtime.Caller(1)
 	rw.logger.WithFields(logrus.Fields{
 		"name": rw.name,
 		"status": "request",
+		"file": filename,
+		"line" : line,
+		"ok": ok,
 	}).Warn("RUnlock()")
 	rw.mu.RUnlock()
 	rw.logger.WithFields(logrus.Fields{
 		"name": rw.name,
 		"status": "grant",
+		"file": filename,
+		"line" : line,
+		"ok": ok,
 	}).Warn("RUnlock()")
 }
 
 func (rw *RWMutex_trace) Lock() {
+	pc, filename, line, ok := runtime.Caller(1)
 	rw.logger.WithFields(logrus.Fields{
 		"name": rw.name,
 		"status": "request",
+		"file": filename,
+		"line" : line,
+		"ok": ok,
 	}).Warn("Lock()")
 	rw.mu.Lock()
 	rw.logger.WithFields(logrus.Fields{
 		"name": rw.name,
 		"status": "grant",
+		"file": filename,
+		"line" : line,
+		"ok": ok,
 	}).Warn("Lock()")
 }
 
 func (rw *RWMutex_trace) Unlock() {
+	pc, filename, line, ok := runtime.Caller(1)
 	rw.logger.WithFields(logrus.Fields{
 		"name": rw.name,
 		"status": "request",
+		"file": filename,
+		"line" : line,
+		"ok": ok,
 	}).Warn("Unlock()")
 	rw.mu.Unlock()
 	rw.logger.WithFields(logrus.Fields{
 		"name": rw.name,
 		"status": "grant",
+		"file": filename,
+		"line" : line,
+		"ok": ok,
 	}).Warn("Unlock()")
 }
